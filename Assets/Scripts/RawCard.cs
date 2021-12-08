91892703177efd93a5384134bbcb1b7c9d606c69
DESKTOP-RN7AOB1\ad2 <ridik60302WP.PL>
2021-12-08 23:41:21 +0100

    Create a raw card and adding the necessary assets

---
+++ Assets/Scripts/RawCard.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Now Card",menuName = "Create Card")]
+public class RawCard : ScriptableObject
+
+{
+
+}
