5f9d8fd4a09f4214403d9165082f3fe835e02ee9
DESKTOP-RN7AOB1\ad2 <ridik60302WP.PL>
2021-12-09 20:01:26 +0100

    little corrections

--- Assets/Scripts/ScriptableObjectsScripts/RawCard.cs
+++ Assets/Scripts/ScriptableObjectsScripts/RawCard.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 [CreateAssetMenu(fileName = "Now Card",menuName = "Create Card")]
 public class RawCard : ScriptableObject
-{
+{ 
     public List<string> TitleList = new List<string>();
     public List<string> DiscriptionList = new List<string>();
-
+   
     public List<Sprite> ImageList = new List<Sprite>();
 
+    public List<ScriptableObject> EffectList = new List<ScriptableObject>();
 }
