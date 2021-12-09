de92f08b351927244c5f933d0c35e108e2d2b7a7
DESKTOP-RN7AOB1\ad2 <ridik60302WP.PL>
2021-12-09 23:20:02 +0100

    poison effect added

---
+++ Assets/Scripts/ScriptableObjectsScripts/PosionEffect.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(fileName = "Posion Effect", menuName = "Create Posion Effect")]
+public class PosionEffect : ScriptableObject
+{
+    public float posionTime;
+    public float damage;
+    public string effectDiscription;
+
+}
