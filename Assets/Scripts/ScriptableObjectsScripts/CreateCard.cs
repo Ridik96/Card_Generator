c279ba0f36998085ae66a23edf3a823185dbb688
DESKTOP-RN7AOB1\ad2 <ridik60302WP.PL>
2021-12-09 01:16:22 +0100

    drawing the title, description and picture

---
+++ Assets/Scripts/ScriptableObjectsScripts/CreateCard.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class CreateCard : MonoBehaviour
+{
+    [SerializeField] private TMPro.TextMeshProUGUI title;
+    [SerializeField] private TMPro.TextMeshProUGUI discripton;
+
+    [SerializeField] private Image potion;
+
+    public RawCard cardData;
+
+    private void Start()
+    {
+        OnCreateCard();
+    }
+    private void OnCreateCard()
+    {
+        int firstNumber;
+        int secondNumber;
+        int thirdNumber;
+
+        firstNumber = Random.Range(0, cardData.TitleList.Count);
+        secondNumber = Random.Range(0, cardData.DiscriptionList.Count);
+        thirdNumber = Random.Range(0, cardData.ImageList.Count);
+
+
+        title.text = cardData.TitleList[firstNumber];
+        discripton.text = cardData.DiscriptionList[secondNumber];
+        potion.sprite = cardData.ImageList[thirdNumber];
+
+    }
+
+}
--- Assets/Scripts/ScriptableObjectsScripts/RawCard.cs
+++ Assets/Scripts/ScriptableObjectsScripts/RawCard.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 [CreateAssetMenu(fileName = "Now Card",menuName = "Create Card")]
 public class RawCard : ScriptableObject
-
 {
+    public List<string> TitleList = new List<string>();
+    public List<string> DiscriptionList = new List<string>();
+
+    public List<Sprite> ImageList = new List<Sprite>();
 
 }
