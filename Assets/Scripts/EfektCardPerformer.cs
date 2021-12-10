c999ae80afb8203075c901aa177358859151dfcf
DESKTOP-RN7AOB1\ad2 <ridik60302WP.PL>
2021-12-11 00:44:16 +0100

    added random effect

---
+++ Assets/Scripts/EfektCardPerformer.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EfektCardPerformer : MonoBehaviour
+{
+    public List<Slider> statBars = new List<Slider>();
+
+    
+}
